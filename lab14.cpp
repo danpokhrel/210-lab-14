270ea295746d9e41ff9a304c69f2f7b28057b955
Dan <texhnistguru@gmail.com>
2024-09-28 21:57:20 -0700

    Commit 3

--- lab14.cpp
+++ lab14.cpp
@@ -1,30 +1,39 @@
 // COMSC-210 | Lab 14 | Dan Pokhrel
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <cstdlib>
 using namespace std;
+
+const int COUNT = 5;
 
 class Color {
 private:
-    char red;
-    char green;
-    char blue;
+    unsigned char red;
+    unsigned char green;
+    unsigned char blue;
 public:
     // Getters and Setters
-    char getRed() const {return red;}
-    void setRed(char r) {red = r;}
-    char getGreen() const {return green;}
-    void setGreen(char g)  {green = g;}
-    char getBlue() const {return blue;}
-    void setBlue(char b) {blue = b;}
+    unsigned char getRed() const {return red;}
+    void setRed(unsigned char r) {red = r;}
+    unsigned char getGreen() const {return green;}
+    void setGreen(unsigned char g)  {green = g;}
+    unsigned char getBlue() const {return blue;}
+    void setBlue(unsigned char b) {blue = b;}
 
     // Formatted Output
     void print() {
-
+        cout << "Color: " << (int)red << "r | " << (int)green << "g | " << (int)blue << "b\n";
     }
 };
 
 int main(){
-    cout << "Hello World";
+    vector<Color> colors;
+
+    for (int i = 0; i < COUNT; i++){
+        Color tmp;
+        tmp.setRed(rand()%255);
+    }
 
     return 0;
 }
