67594a2b48a14c33c8b1959d1ec906de9bdb8106
Dan <texhnistguru@gmail.com>
2024-09-28 21:47:19 -0700

    Commit 2

--- lab14.cpp
+++ lab14.cpp
@@ -1,6 +1,27 @@
 // COMSC-210 | Lab 14 | Dan Pokhrel
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+class Color {
+private:
+    char red;
+    char green;
+    char blue;
+public:
+    // Getters and Setters
+    char getRed() const {return red;}
+    void setRed(char r) {red = r;}
+    char getGreen() const {return green;}
+    void setGreen(char g)  {green = g;}
+    char getBlue() const {return blue;}
+    void setBlue(char b) {blue = b;}
+
+    // Formatted Output
+    void print() {
+
+    }
+};
 
 int main(){
     cout << "Hello World";
